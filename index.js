be344c4c8815c22e7566ab691f359c23ec5c17db
Zeon <zeonstewart23@gmail.com>
2021-08-10 21:58:39 -0400

    commit tree 6dc79610164b90a5d684cef244421616bb635099

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(list = [sally, mark, jack, zeon]) {
+    return list.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(newList = [marry, keefer, noah, paige]) {
+    return newList.slice(2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = i => {
+    return function(fare) {
+        return fare * i;
+    }
+
+}
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (anArray = [tom, sarah, ashley], returnFirstTwoDrivers) => {
+
+    return returnFirstTwoDrivers(anArray);
+}
